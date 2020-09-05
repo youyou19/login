bd15d97f0adb565a15174040b1add91afe10eebd
youyou19 <juleswilfrid9@gmail.com>
2020-09-05 10:47:10 -0400

    Initial commit

---
+++ app/src/main/java/com/miu/login/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.miu.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+
+class MainActivity() : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
